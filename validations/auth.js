ff94fdbfc446d95396ecd7b430b4e92294b6ef93
vakor <myksv200@gmail.com>
2024-05-17 16:27:33 +0300

    Auth validations

---
+++ validations/auth.js
@@ -0,0 +1,23 @@
+import {body} from "express-validator";
+import {isImage} from "../utils/index.js";
+import {VALIDATION_PARAMS} from "../config/constants.js";
+const {username, password} = VALIDATION_PARAMS
+
+const errorTexts = {
+    'username': `Username must be between ${username.minLength} and ${username.maxLength} characters`,
+    'email': 'Invalid email',
+    'password': `Password must be between ${password.minLength} and ${password.maxLength} characters`,
+    'avatarUrl': 'Invalid URL'
+}
+
+export const signupValidations = [
+    body('username', errorTexts.username).isLength({min: username.minLength, max: username.maxLength}),
+    body('email', errorTexts).isEmail(),
+    body('password', errorTexts.password).isLength({min: password.minLength, max: password.maxLength}),
+    body('avatarUrl', errorTexts.avatarUrl).optional().isURL().custom(isImage),
+]
+
+export const loginValidations = [
+    body('email', errorTexts).isEmail(),
+    body('password', errorTexts.password).isLength({min: password.minLength, max: password.maxLength}),
+]
