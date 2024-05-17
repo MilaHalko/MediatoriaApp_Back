b0a4474470d498f31cbfff35054b9c75a045a610
vakor <myksv200@gmail.com>
2024-05-18 00:22:11 +0300

    Validation Error returning

--- utils/handleValidationErrors.js
+++ utils/handleValidationErrors.js
@@ -1,10 +1,14 @@
 import {validationResult} from "express-validator";
 
 export const handleValidationErrors = (req, res, next) => {
+
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-        console.log(errors)
-        return res.status(400).json(errors.array())
+        console.log(errors.array()[0].msg)
+        return res.status(400).json({
+            message: errors.array()[0].msg
+        })
     }
+
     next()
 }
